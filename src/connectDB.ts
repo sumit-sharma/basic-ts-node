835611c2d84a707c3fc9da5500e588f619d80b1f
sumit-sharma <211.sumit@gmail.com>
2024-01-23 16:48:17 +0530

    connection settings

--- src/connectDB.ts
+++ src/connectDB.ts
@@ -1,11 +1,8 @@
-const mongoose = require("mongoose");
+import { Schema, model, connect } from 'mongoose';
 
 export const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    const conn = await connect(String(process.env.MONGO_URI));
 
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error:any) {
